c91debb4644688604e722332f59084eccfa7f4d8
Julien <julienhuynh98@gmail.com>
2020-03-13 11:45:08 +0100

    TP2LP2

--- TP2 - Q2LP2.py
+++ TP2 - Q2LP2.py
@@ -13,7 +13,7 @@ p1 += 2 * x1 + 7 * x2 + 2 * x3
 #Add constraints
 p1 += 1 * x1 + 4 * x2 + 1 * x3 >= 10.0, "First constraint"
 p1 += 4 * x1 + 2 * x2 + 2 * x3 >= 13, "Second constraint"
-p1 += 1 * x1 + 1 * x2 - 1 * x3 >= 0, "Second constraint"
+p1 += 1 * x1 + 1 * x2 - 1 * x3 >= 0, "Third constraint"
 #Write LP into a .lp file
 p1.writeLP("TP2LP2.lp")
 #Solve the LP
@@ -26,3 +26,30 @@ for v in p1.variables():
     print(v.name, "=", v.varValue)
 #Objective function value
 print("Objective function value = ", pulp.value(p1.objective))
+
+#Integer part
+
+#Initialize variables
+p2 = pulp.LpProblem("First problem", pulp.LpMinimize)
+#Initialize variables, Use LpContinuous for R, LpInteger
+x1 = pulp.LpVariable("x1", 0, None, pulp.LpInteger)
+x2 = pulp.LpVariable("x2", 0, None, pulp.LpInteger)
+x3 = pulp.LpVariable("x3", 0, None, pulp.LpInteger)
+#Add the objective function
+p2 += 2 * x1 + 7 * x2 + 2 * x3
+#Add constraints
+p2 += 1 * x1 + 4 * x2 + 1 * x3 >= 10.0, "First constraint"
+p2 += 4 * x1 + 2 * x2 + 2 * x3 >= 13, "Second constraint"
+p2 += 1 * x1 + 1 * x2 - 1 * x3 >= 0, "Third constraint"
+#Write LP into a .lp file
+p2.writeLP("TP2LP2.lp")
+#Solve the LP
+#Another solver can be used by inserting it into the parenthesis ex : p1.solve(CPLEX())
+p2.solve()
+#Print status of the solution
+print("Status:", pulp.LpStatus[p2.status])
+#Print solution variables
+for v in p2.variables():
+    print(v.name, "=", v.varValue)
+#Objective function value
+print("Objective function value = ", pulp.value(p2.objective))
