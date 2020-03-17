54a516fe847b91e6646bd0b4d80d4ab5cc55648a
Julien <julienhuynh98@gmail.com>
2020-03-17 18:04:24 +0100

    LP working

--- TP2-Elections.py
+++ TP2-Elections.py
@@ -3,27 +3,32 @@ import matplotlib.pyplot as plt
 import pulp
 import csv
 
-import csv
+N = 538
 
 with open('DataElection.csv', newline='') as f:
     reader = csv.reader(f)
     pop = list(reader)
-alpha = []
-for i in range(52) :
-    alpha.append("alpha" + str(i + 1))
-print(alpha)
+alphalab = []
+for i in range(len(pop)) :
+    alphalab.append("alpha_" + str(i + 1))
+
 #Initialize variables
 p1 = pulp.LpProblem("US Elections 2016", pulp.LpMinimize)
 #Initialize variables, Use LpContinuous for R, LpInteger
 u = pulp.LpVariable("u", 0, None, pulp.LpContinuous)
 v = pulp.LpVariable("v", 0, None, pulp.LpContinuous)
-for j in alpha :
+alpha = []
+for j in alphalab :
     j = pulp.LpVariable(str(j), 0, None, pulp.LpInteger)
+    alpha.append(j)
 #Add the objective function
 p1 += u - v
 #Add constraints
-p1 += 1 * x1 + 1 * x2 <= 6.0, "First constraint"
-p1 += 9 * x1 + 5 * x2 <= 45.0, "Second constraint"
+for j in range(len(alphalab)) :
+    p1 += v - alpha[j] * 1e6 / float(pop[j][0]) <= 0
+    p1 += alpha[j] * 1e6 / float(pop[j][0]) - u <=0
+#Add the last equality constraint
+    p1 += sum(alpha) == N
 #Write LP into a .lp file
 p1.writeLP("TP2LP1.lp")
 #Solve the LP
